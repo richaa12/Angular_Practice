3e56636c740d03e52db27267ac76059634c7e2bd
tkmagesh <tkmagesh77@gmail.com>
2015-05-27 20:50:48 +0530

    Memoization

---
+++ Day-03/memoization.js
@@ -0,0 +1,17 @@
+//Finding if a given number is a prime number or not
+
+function getPrimeFinder(){
+    var cache = {};
+    function isPrime(n){
+        console.log("processing for ", n);
+        if (n < 3) return true;
+        for (var i=2; i <= (n/2); i++)
+            if (n % i === 0) return false;
+        return true;
+    }
+    return function (n){
+        if (typeof cache[n] === "undefined")
+            cache[n] = isPrime(n);
+        return cache[n];
+    }
+}
